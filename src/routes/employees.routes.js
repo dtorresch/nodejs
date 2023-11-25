5fdbf47c1e8f1835bc4fa7b0d8c874272a149a9d
dtorresch <109763252+dtorresch@users.noreply.github.com>
2023-11-25 00:21:08 -0500

    Update employees.routes.js
    tabla Account

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -5,9 +5,17 @@ import {
   getEmployee,
   getEmployees,
   updateEmployee,
+  getAccount,
+  getAccounts,
 } from "../controllers/employees.controller.js";
 
 const router = Router();
+
+// GET all Account
+router.get("/account", getAccounts);
+
+// GET An Account
+router.get("/account/:id", getAccount);
 
 // GET all Employees
 router.get("/employees", getEmployees);
