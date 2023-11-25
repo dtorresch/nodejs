14a4c0764954f8e499b28f1b08452a4b7a0f053c
dtorresch <109763252+dtorresch@users.noreply.github.com>
2023-11-25 14:18:37 -0500

    Update employees.controller.js

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,26 +1,7 @@
 import { pool } from "../db.js";
-import bcrypt from "bcrypt";
 
 //Post login
-export const login = async (req, res) => {
-  try {
-    const { dni, clave } = req.body;
-    const [rows] = await pool.query("SELECT * FROM usuario WHERE dni = ?", [dni]);
-    if (rows.length <= 0) {
-      return res.status(404).json({ message: "Usuario not found" });
-    }
-    const user = rows[0];
-    // Comparar la contraseña ingresada con la almacenada en la base de datos
-    const isPasswordValid = await bcrypt.compare(clave, user.clave);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-    res.json({ message: "Login successful" });
-  } catch (error) {
-    return res.status(500).json({ message: "Something goes wrong" });
-  }
-};
-//end login
+
 
 //###################################3
 export const getEmployees = async (req, res) => {
