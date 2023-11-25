5c0699c05567184376cbf9dbb216da7629b91428
dtorresch <109763252+dtorresch@users.noreply.github.com>
2023-11-25 00:23:19 -0500

    Update employees.controller.js
    Account

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,5 +1,31 @@
 import { pool } from "../db.js";
+//getAccounts
+export const getAccounts = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM Account");
+    res.json(rows);
+  } catch (error) {
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+//getAccount
+export const getAccount = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [rows] = await pool.query("SELECT * FROM Account WHERE id = ?", [
+      id,
+    ]);
 
+    if (rows.length <= 0) {
+      return res.status(404).json({ message: "Account not found" });
+    }
+
+    res.json(rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+//
 export const getEmployees = async (req, res) => {
   try {
     const [rows] = await pool.query("SELECT * FROM employee");
