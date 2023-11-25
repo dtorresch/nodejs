4805c095172a517f73d865f7cac52352e2af7897
dtorresch <109763252+dtorresch@users.noreply.github.com>
2023-11-24 21:50:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//import express from 'express'
+const http = require('http');
+const PORT = 3000;
+
+//const app = express()
+//app.get('/usuarios',(re,res) => res.send('obteniendo usuarios'))
+//app.post('/usuarios',(re,res) => res.send('creando usuarios'))
+//app.put('/usuarios',(re,res) => res.send('actualizando usuarios'))
+//app.delete('/usuarios',(re,res) => res.send('eliminando usuarios'))
+//app.list(3000)
+//console.log('ejecutando el puerto del servidor')
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
