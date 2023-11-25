3dcf3396a84b80ccc7df62f469a9fb20f90ead9b
dtorresch <109763252+dtorresch@users.noreply.github.com>
2023-11-24 21:46:23 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-config();
-
-export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "db4free.net";
-export const DB_USER = process.env.DB_USER || "prueba002";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "T76Ls%P-YnV";
-export const DB_DATABASE = process.env.DB_DATABASE || "prueba002";
-export const DB_PORT = process.env.DB_PORT || 3306;
